5911eea7b76bba85058937c1d7ca70608024014d
SHINSUNGJIN <pxp13716@gmail.com>
2022-07-29 13:42:24 +0900

    .

--- chap11_redux/src/modules/counterR.js
+++ chap11_redux/src/modules/counterR.js
@@ -14,6 +14,33 @@ const init = {
   storeName: 'Counter Store',
   num: 0,
 }
+const counterR = (state=init, action) => {
+  switch (action.type) {
+    case COUNTER_INCREASE:
+      return {...state, num: state.num + action.payload}
+    case COUNTER_DECREASE:
+      return {...state, num: state.num - 1};
+    default:
+      return state;
+  }
+}
+export default counterR;
+
+/*
+const COUNTER_INCREASE = 'COUNTER_INCREASE';
+const COUNTER_DECREASE = 'COUNTER_DECREASE';
+
+export const increaseAction = num => {
+  let value = Number(num);
+  if(isNaN(value)) value = 0;
+  return {type: COUNTER_INCREASE, payload: value}
+}
+export const decreaseAction = () => ({type: COUNTER_DECREASE});
+
+const init = {
+  storeName: 'Counter Store',
+  num: 0,
+}
 // dispatch({type: 'COUNTER_INCREASE', payload: 2})
 const counterR = (state=init, action) => {
   switch (action.type) {
@@ -25,5 +52,5 @@ const counterR = (state=init, action) => {
       return state;
   }
 }
-
 export default counterR;
+*/
