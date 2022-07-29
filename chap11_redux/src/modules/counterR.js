75ab3d09f843e20fc66b9df384cbd8ae091620f8
SHINSUNGJIN <pxp13716@gmail.com>
2022-07-29 13:47:52 +0900

    .

--- chap11_redux/src/modules/counterR.js
+++ chap11_redux/src/modules/counterR.js
@@ -1,29 +1,30 @@
 // counterR.js
+// redux-actions
+import {createAction, handleActions} from 'redux-actions'
 
 const COUNTER_INCREASE = 'COUNTER_INCREASE';
 const COUNTER_DECREASE = 'COUNTER_DECREASE';
 
-export const increaseAction = num => {
+export const increaseAction = createAction(COUNTER_INCREASE, num => {
   let value = Number(num);
   if(isNaN(value)) value = 0;
-  return {type: COUNTER_INCREASE, payload: value}
-}
-export const decreaseAction = () => ({type: COUNTER_DECREASE});
+  return value;       // 이 값을 action의 payload 라는 이름으로 넣어준다.
+});
+export const decreaseAction = createAction(COUNTER_DECREASE)
 
 const init = {
   storeName: 'Counter Store',
   num: 0,
 }
-const counterR = (state=init, action) => {
-  switch (action.type) {
-    case COUNTER_INCREASE:
-      return {...state, num: state.num + action.payload}
-    case COUNTER_DECREASE:
-      return {...state, num: state.num - 1};
-    default:
-      return state;
+const counterR = handleActions({
+  [COUNTER_INCREASE]: (state, action) => {
+    return {...state, num: state.num + action.payload}
+  },
+  [COUNTER_DECREASE]: (state, action) => {
+    return {...state, num: state.num - 1};
   }
-}
+}, init);
+
 export default counterR;
 
 /*
