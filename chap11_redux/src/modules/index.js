11b31903b0002089e1f77230dedd367ba18f94b0
SHINSUNGJIN <pxp13716@gmail.com>
2022-07-29 11:45:02 +0900

    .

---
+++ chap11_redux/src/modules/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import counterR from './counterR'
+import todoListR from './todoListR'
+
+const rootReducer = combineReducers({
+  counterR, todoListR, 
+});
+export default rootReducer;
