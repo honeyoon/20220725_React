6f0579c975d74d7b5d633a7d55249a6b751c59f9
SHINSUNGJIN <pxp13716@gmail.com>
2022-07-29 09:52:10 +0900

    .

--- chap09_context/src/modules/TodoListContext.js
+++ chap09_context/src/modules/TodoListContext.js
@@ -1,2 +1,23 @@
 // TodoListContext.js
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
+
+const TodoListContext = createContext({
+  state: {todoList: [], text: ''},
+  action: {
+    updateTodoList: id => {},
+    deleteTodoList: id => {},
+    addTodoList: text => {},
+    changeText: text => {}
+  }
+});
+
+function TodoListProvider(props) {
+  return (
+    <TodoListContext.Provider>
+      {props.children}
+    </TodoListContext.Provider>   
+  )
+}
+const TodoListConsumer = TodoListContext.Consumer;
+
+export {TodoListProvider, TodoListConsumer}
